c92233939f245ffa058650295fac9255d71139fe
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-13 15:27:03 +0200

    add 2d variant of node server realization

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ const http = require('http');
 // 2 variant
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('or any other data');
-}).listen(8080, 'ipAdress - localhost by default - so I can not to write it', () => console.log('server is working'));
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+        <!doctype>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Node Server Example</title>
+            </head>
+            <body>
+              <h1>Node Server Example</h1>
+            </body>
+        </html> 
+    `);
+}).listen(8080, () => console.log('server is working'));
