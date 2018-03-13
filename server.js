85a40e53f58beae3a7ffdb8ba1d7a758dc97d0b2
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-13 15:14:31 +0200

    refactored prev code

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const http = require('http');
 
 // 1 variant
 
-const server = http.createServer();
+// const server = http.createServer();
+//
+// server.on('request', (req, res) => {
+//     res.writeHead(200, { 'Content-Type' : 'text/plain' });
+//     res.end('or any other data');
+// });
+//
+// server.listen(8080, 'ipAdress - localhost by default - so I can not to write it', () => console.log('server is working'));
 
-server.on('request', (req, res) => {
-    res.end('or any other data')
-});
 
-server.listen(8080, 'ipAdress - localhost by default', () => console.log('server is working'));
+// 2 variant
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('or any other data');
+}).listen(8080, 'ipAdress - localhost by default - so I can not to write it', () => console.log('server is working'));
