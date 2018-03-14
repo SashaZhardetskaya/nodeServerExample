e2dd73ce427d4c66d7fe0c47376356db802b7275
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-14 11:14:14 +0200

    finished postserver

--- fileserver.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-http.createServer()
--- public/app.js
+++
@@ -1,2 +0,0 @@
-const button = document.querySelector('button');
-button.addEventListener('click', event => alert('Node.js в действии'));
